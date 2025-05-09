83bdbdc6956acf89177610c8f88f0811f8c415af
Andy Gimma <andrewgimma@protonmail.com>
2025-05-09 10:08:34 -0400

    Remove unused context

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import Navbar from "./components/Navbar/Navbar";
 
 import HomePage from "./pages/HomePage";
 import LocationDetailsPage from "./pages/LocationDetailsPage";
-import { WeatherProvider } from "./context/weatherContext";
 import SearchPage from "./pages/SearchPage";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
-    <WeatherProvider>
+    <>
       <Navbar />
       <SearchBar />
       <Routes>
@@ -18,7 +17,7 @@ function App() {
         <Route path="/details" element={<LocationDetailsPage />} />
         <Route path="/search/:lat/:long" element={<SearchPage />} />
       </Routes>
-    </WeatherProvider>
+    </>
   );
 }
 
