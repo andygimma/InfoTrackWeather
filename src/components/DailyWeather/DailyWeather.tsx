763edc155f49da6341be0bb2628fe7685677f54c
Andy Gimma <andrewgimma@protonmail.com>
2025-05-13 19:09:40 -0400

    Remove formatDate

--- src/components/DailyWeather/DailyWeather.tsx
+++ src/components/DailyWeather/DailyWeather.tsx
@@ -89,21 +89,4 @@ function DailyWeather(props: IDailyWeather) {
   );
 }
 
-function formatDate(dateString: Date) {
-  const date = new Date(dateString);
-
-  const day: string = date.getDate().toString().padStart(2, "0");
-  const month: string = (date.getMonth() + 1).toString().padStart(2, "0");
-
-  let hours: number = date.getHours();
-  const minutes: string = date.getMinutes().toString().padStart(2, "0");
-  const ampm: string = hours >= 12 ? "pm" : "am";
-
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  const hourString: string = hours.toString().padStart(2, "0");
-
-  return `${hourString}:${minutes}${ampm}`;
-}
-
 export default DailyWeather;
