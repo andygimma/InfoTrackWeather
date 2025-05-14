c024670319025a878ee2bc87a036000552720302
Andy Gimma <andrewgimma@protonmail.com>
2025-05-14 09:30:12 -0400

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import SearchBar from "./components/SearchBar/SearchBar";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <SearchBar />
       <Routes>
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/search/:lat/:long" element={<SearchPage />} />
         <Route path="*" element={<div>404 error</div>} />
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+function AboutPage() {
+  return (
+    <main className="flex flex-col items-center justify-center p-4">
+      <h1 className="text-4xl underline mt-4">How to use this app</h1>
+      <h2 className="text-3xl mt-8">Get local weather</h2>
+      <p>
+        On the home page, simply allow the app to get your browser location.
+      </p>
+    </main>
+  );
+}
+
+export default AboutPage;
